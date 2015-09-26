819fe6e09a851b4cd084a85ae4c8baf8d1c29cf8
LiHao <770753119@qq.com>
2015-09-26 17:28:34 +0800

    delete js

--- js/lihao-blog.js
+++ js/lihao-blog.js
@@ -21,18 +21,18 @@ $(function() {
 // });
 
 // responsive tables
-$(document).ready(function() {
-	$("table").wrap("<div class='table-responsive'></div>");
-	$("table").addClass("table");
-});
+// $(document).ready(function() {
+// 	$("table").wrap("<div class='table-responsive'></div>");
+// 	$("table").addClass("table");
+// });
 
 // responsive embed videos
-$(document).ready(function () { 
-    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
-    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
-});
+// $(document).ready(function () { 
+//     $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+// 	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+//     $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+// 	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+// });
 
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
